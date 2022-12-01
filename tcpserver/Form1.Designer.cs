7eec921538ce168715cbf7004b46f93aae381a54
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-12-01 21:27:52 +0200

    Add project files.

---
+++ tcpserver/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace tcpserver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbConnections = new System.Windows.Forms.ListBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbConnections
+            // 
+            this.lbConnections.FormattingEnabled = true;
+            this.lbConnections.Location = new System.Drawing.Point(31, 34);
+            this.lbConnections.Name = "lbConnections";
+            this.lbConnections.Size = new System.Drawing.Size(211, 199);
+            this.lbConnections.TabIndex = 0;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(31, 15);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(84, 13);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "my ip address is:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(291, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Listen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbStatus
+            // 
+            this.tbStatus.Location = new System.Drawing.Point(249, 34);
+            this.tbStatus.Multiline = true;
+            this.tbStatus.Name = "tbStatus";
+            this.tbStatus.Size = new System.Drawing.Size(377, 199);
+            this.tbStatus.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 256);
+            this.Controls.Add(this.tbStatus);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lbConnections);
+            this.Name = "Form1";
+            this.Text = "tcp server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbConnections;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbStatus;
+    }
+}
+
---
+++ tcpserver/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+namespace tcpserver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private ArrayList alSockets;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //IPHostEntry IPHost = Dns.GetHostByName(Dns.GetHostName());
+            IPHostEntry IPHost = Dns.GetHostEntry(Dns.GetHostName());
+            Text = IPHost.AddressList[0].ToString();
+            lblStatus.Text = "My IP address is " + IPHost.AddressList[0].ToString();
+            alSockets = new ArrayList();
+            Thread thdListener = new Thread(new
+            ThreadStart(listenerThread));
+            thdListener.Start();
+        }
+
+        public void listenerThread()
+        {
+
+            IPHostEntry IPHost = Dns.GetHostEntry(Dns.GetHostName());
+            //TcpListener tcpListener = new TcpListener(8080);
+            TcpListener tcpListener = new TcpListener(IPHost.AddressList[0], 8080);
+            tcpListener.Start();
+            while (true)
+            {
+                Socket handlerSocket = tcpListener.AcceptSocket();
+                if (handlerSocket.Connected)
+                {
+                    lbConnections.Items.Add(
+                    handlerSocket.RemoteEndPoint.ToString() + " connected."
+                    );
+                    lock (this)
+                    {
+                        alSockets.Add(handlerSocket);
+                    }
+                    ThreadStart thdstHandler = new ThreadStart(handlerThread);
+                    Thread thdHandler = new Thread(thdstHandler);
+                    thdHandler.Start();
+                }
+            }
+        }
+
+
+        public void handlerThread()
+        {
+            Socket handlerSocket = (Socket)alSockets[alSockets.Count - 1];
+            NetworkStream networkStream = new
+            NetworkStream(handlerSocket);
+            int thisRead = 0;
+            int blockSize = 1024;
+            Byte[] dataByte = new Byte[blockSize];
+            lock (this)
+            {
+                // Only one process can access
+                // the same file at any given time
+                Stream fileStream = File.OpenWrite("c:\\my documents\\upload.txt");
+                while (true)
+                {
+                    thisRead = networkStream.Read(dataByte, 0, blockSize);
+                    fileStream.Write(dataByte, 0, thisRead);
+                    if (thisRead == 0) break;
+                }
+                fileStream.Close();
+            }
+            lbConnections.Items.Add("File Written");
+            handlerSocket = null;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int bytesReceived = 0;
+            byte[] recv = new byte[1];
+            Socket clientSocket;
+            Socket listenerSocket = new Socket(
+            AddressFamily.InterNetwork,
+            SocketType.Stream,
+            ProtocolType.Tcp
+            );
+            IPHostEntry IPHost = Dns.GetHostEntry(Dns.GetHostName());
+            //TcpListener tcpListener = new TcpListener(8080);
+            TcpListener tcpListener = new TcpListener(IPHost.AddressList[0], 8080);
+            //IPHostEntry IPHost = Dns.GetHostByName(Dns.GetHostName());
+            IPEndPoint ipepServer = new
+            IPEndPoint(IPHost.AddressList[0], 8080);
+            listenerSocket.Bind(ipepServer);
+            listenerSocket.Listen(-1);
+            clientSocket = listenerSocket.Accept();
+            if (clientSocket.Connected)
+            {
+                do
+                {
+                    bytesReceived = clientSocket.Receive(recv);
+                    tbStatus.Text += Encoding.ASCII.GetString(recv);
+                }
+
+                while (bytesReceived != 0);
+            }
+        }
+
+        
+        
+        
+
+
+
+    }
+}
